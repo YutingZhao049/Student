dfb985601ff476ea454a1e1968fede4f16d430ec
29685 <29685@LAPTOP-K5EFA86I>
2020-09-22 15:34:30 +0800

    Added an index page

--- Student.php
+++ Student.php
@@ -19,7 +19,6 @@ $this->emails = array();
 $this->grades = array();
 }
 
-}
 
 function add_email($which,$address) {
 $this->emails[$which] = $address;
@@ -27,3 +26,23 @@ $this->emails[$which] = $address;
 function add_grade($grade) {
 $this->grades[] = $grade;
 }
+
+
+function average() {
+$total = 0;
+foreach ($this->grades as $value)
+$total += $value;
+return $total / count($this->grades);
+}
+
+function toString() {
+$result = $this->first_name . ' ' . $this->surname;
+$result .= ' ('.$this->average().")\n";
+foreach($this->emails as $which=>$what)
+$result .= $which . ': '. $what. "\n";
+$result .= "\n";
+return '<pre>'.$result.'</pre>';
+}
+}
+
+
